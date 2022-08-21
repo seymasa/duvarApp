852d57e22a91cd944244ec9c057061f79c2b4629
Seyma SARIGİL <seymasargil@gmail.com>
2022-08-21 12:28:22 +0300

    The bug in the model api return has been resolved.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ app.controller("duvarController", function($scope, $http) {
     $scope.author = "Yeni Kullanıcı";
     $scope.newAuthorName = null;
     $scope.newMessage = null;
-    $scope.tags = ["Siyasi Zorbalık", "Irkçılık", "Akran Zorbalığı", "Cinsiyetçilik", "Kızdırma"];
+    $scope.tags = ["Irkçılık", "Cinsiyetçilik", "Kızdırma", "Nötr"];
 
     $scope.sampleMessages = [
         {
@@ -17,59 +17,6 @@ app.controller("duvarController", function($scope, $http) {
                 {
                     label: "Cinsiyetçilik",
                     score:92.44
-                }
-            ]
-        },
-        {
-            id: "aLrT9cgK2Z3o",
-            author:"Test Kullanıcı 3",
-            message:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis scelerisque commodo sem, non commodo ex egestas a. Nullam finibus purus sem, sit amet ornare purus cursus in. Phasellus sit amet lacus ut lectus scelerisque facilisis a quis nibh. Suspendisse eu vestibulum.",
-            tags:[
-                {
-                    label: "Saldırganlık",
-                    score:73.28
-                },
-                {
-                    label: "Irkçılık",
-                    score:76.99
-                },
-                {
-                    label: "Cinsiyetçilik",
-                    score:67
-                }
-            ]
-        },
-        {
-            id: "i4xkTa7ZscEZ",
-            author:"Test Kullanıcı 4",
-            message:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dictum massa nisl. Aliquam a lacinia elit. Morbi sed bibendum nisi. Donec consectetur iaculis elementum. Proin sollicitudin lacinia feugiat. Nullam tincidunt viverra ex vitae.",
-            tags:[
-                {
-                    label: "Irkçılık",
-                    score:80.32
-                },
-                {
-                    label: "Cinsiyetçilik",
-                    score:92.44
-                }
-            ]
-        },
-        {
-            id: "aLrT9cgK2Z3G",
-            author:"Test Kullanıcı 4",
-            message:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis scelerisque commodo sem, non commodo ex egestas a. Nullam finibus purus sem, sit amet ornare purus cursus in. Phasellus sit amet lacus ut lectus scelerisque facilisis a quis nibh. Suspendisse eu vestibulum.",
-            tags:[
-                {
-                    label: "Saldırganlık",
-                    score:73.28
-                },
-                {
-                    label: "Irkçılık",
-                    score:76.99
-                },
-                {
-                    label: "Cinsiyetçilik",
-                    score:67
                 }
             ]
         },
