22c1c3b2002ae0541792fb0e3f8757d061ea73b9
Seyma SARIGİL <seymasargil@gmail.com>
2022-08-21 16:01:26 +0300

    updated server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/duvarApp'));
+app.use(express.static(__dirname + '/'));
 
 app.listen(process.env.PORT || 8080);
 
