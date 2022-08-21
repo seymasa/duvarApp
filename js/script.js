d9f1e260d3c7d20af55ad529e74baa661c0ed29d
Seyma SARIGİL <seymasargil@gmail.com>
2022-08-21 12:22:49 +0300

    The insertnewmessage function has been edited. Updated 'value' words in script.js and index.html to 'score'.

--- js/script.js
+++ js/script.js
@@ -6,21 +6,70 @@ app.controller("duvarController", function($scope, $http) {
 
     $scope.sampleMessages = [
         {
+            id: "i4xkTa7ZscEp",
+            author:"Test Kullanıcı 2",
+            message:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dictum massa nisl. Aliquam a lacinia elit. Morbi sed bibendum nisi. Donec consectetur iaculis elementum. Proin sollicitudin lacinia feugiat. Nullam tincidunt viverra ex vitae.",
+            tags:[
+                {
+                    label: "Irkçılık",
+                    score:80.32
+                },
+                {
+                    label: "Cinsiyetçilik",
+                    score:92.44
+                }
+            ]
+        },
+        {
+            id: "aLrT9cgK2Z3o",
+            author:"Test Kullanıcı 3",
+            message:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis scelerisque commodo sem, non commodo ex egestas a. Nullam finibus purus sem, sit amet ornare purus cursus in. Phasellus sit amet lacus ut lectus scelerisque facilisis a quis nibh. Suspendisse eu vestibulum.",
+            tags:[
+                {
+                    label: "Saldırganlık",
+                    score:73.28
+                },
+                {
+                    label: "Irkçılık",
+                    score:76.99
+                },
+                {
+                    label: "Cinsiyetçilik",
+                    score:67
+                }
+            ]
+        },
+        {
+            id: "i4xkTa7ZscEZ",
+            author:"Test Kullanıcı 4",
+            message:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dictum massa nisl. Aliquam a lacinia elit. Morbi sed bibendum nisi. Donec consectetur iaculis elementum. Proin sollicitudin lacinia feugiat. Nullam tincidunt viverra ex vitae.",
+            tags:[
+                {
+                    label: "Irkçılık",
+                    score:80.32
+                },
+                {
+                    label: "Cinsiyetçilik",
+                    score:92.44
+                }
+            ]
+        },
+        {
             id: "aLrT9cgK2Z3G",
             author:"Test Kullanıcı 4",
             message:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis scelerisque commodo sem, non commodo ex egestas a. Nullam finibus purus sem, sit amet ornare purus cursus in. Phasellus sit amet lacus ut lectus scelerisque facilisis a quis nibh. Suspendisse eu vestibulum.",
             tags:[
                 {
                     label: "Saldırganlık",
-                    value:73.28
+                    score:73.28
                 },
                 {
                     label: "Irkçılık",
-                    value:76.99
+                    score:76.99
                 },
                 {
                     label: "Cinsiyetçilik",
-                    value:67
+                    score:67
                 }
             ]
         },
@@ -44,13 +93,24 @@ app.controller("duvarController", function($scope, $http) {
     };
 
     $scope.insertNewMessage = function () {
-        $scope.sampleMessages.unshift({
-            id: Date.now().toString(),
-            author: $scope.author,
-            message: $scope.newMessage,
-            tags: $scope.randomTagGenerator()
+        $http({
+            method: 'POST',
+            url: 'https://api-inference.huggingface.co/models/nanelimon/bert-base-turkish-bullying',
+            data: { inputs: $scope.newMessage }
+        }).then(function(response){
+            let res = response.data[0];
+            $.each(res, function (k,v) {
+                v.score = parseFloat(( v.score*100).toFixed(2));
+            });
+            $scope.sampleMessages.unshift({
+                id: Date.now().toString(),
+                author: $scope.author,
+                message: $scope.newMessage,
+                tags: res
+            });
+        }, function(err){
+            console.error(err);
         });
-        $scope.newMessage = null;
     };
 
     $scope.setNewAuthorName = function () {
@@ -67,7 +127,7 @@ app.controller("duvarController", function($scope, $http) {
         for (let i = 0; i < tagCount; i++) {
             tags.push({
                 label: $scope.tags[Math.floor(Math.random() * $scope.tags.length)],
-                value: Math.floor(Math.random() * 100)
+                score: Math.floor(Math.random() * 100)
             })
         }
 
